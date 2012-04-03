17d935504540a3ecbf3193b745c1bd897f87065a
Tor Fredrik Eriksen <tor.fredrik.eriksen@gmail.com>
2012-04-03 16:50:09 +0200

    startup klasser til fastintro frameworket

---
+++ FastIntroFramework/Scene.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace FastIntroFramework
+{
+    // a scene is played in sequence, rendering is done within the class inheriting from scene
+    class Scene
+    {
+        public double Duration
+        {
+            get;
+            set;
+        }
+
+        protected void OnLoad(EventArgs e)
+        {
+            
+        }
+
+        protected void OnResize(EventArgs e)
+        {
+            
+        }
+
+        protected void OnUpdateFrame(FrameEventArgs e)
+        {
+            
+            _time += e.Time;
+            //_shader["time"] = (float)_time;
+        }
+
+        protected void OnRenderFrame(FrameEventArgs e)
+        {
+
+        }
+
+        protected void SetupCamera()
+        {
+            
+        }
+    }
+}
---
+++ FastIntroFramework/Timeline.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FastIntroFramework
+{
+    //Class that manages the timeline of the intro, scene order, rendering order, sound playback order etc
+    public class Timeline
+    {
+        public LinkedList<Scene> Scenelist
+        {
+            get;
+            private set;
+        }
+
+        public void addScene(Scene scene)
+        {
+            Scenelist.AddLast(scene);
+        }
+
+        public Scene getScene(double Time)
+        {
+            double currentTime = 0;
+            foreach (Scene scene in Scenelist) {
+                currentTime += scene.Duration;
+                if (Time < currentTime) return scene;
+            }
+            return null;
+        }
+        
+    }
+}
